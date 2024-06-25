ab5ae046e7e20952e6023e3657318a2863b9442d
seolbin <se01bin@suwon.ac.kr>
2024-06-25 21:30:27 +0900

    feat: /chat 구현

--- src/main/java/com/benny/socketApp/controller/ChatController.java
+++ src/main/java/com/benny/socketApp/controller/ChatController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ChatController {
 
     @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/chat")
     public String chat() {
         return "chat";
     }
